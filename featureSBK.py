097b62e5d718bf7ce36ca410807164cb19419622
ShubhamKanhekar <shubhamkanhekar123@gmail.com>
2023-10-20 23:30:52 +0530

    checkpoint 1: bigram dict setup

--- featureSBK.py
+++
@@ -1,8 +0,0 @@
-'''This is the makemore program created from scratch by Shubham B. Kanhekar.
-ref: https://youtu.be/PaCmpygFfXo
-ch: Andrej Karpathy
-vid: The spelled-out intro to language modeling: building makemore
-
-'''
-
-
