f91e92200c5f681d7460e1477674952fec776f12
ShubhamKanhekar <shubhamkanhekar123@gmail.com>
2023-10-20 16:20:46 +0530

    add featureSBK.py

---
+++ featureSBK.py
@@ -0,0 +1,8 @@
+'''This is the makemore program created from scratch by Shubham B. Kanhekar.
+ref: https://youtu.be/PaCmpygFfXo
+ch: Andrej Karpathy
+vid: The spelled-out intro to language modeling: building makemore
+
+'''
+
+
